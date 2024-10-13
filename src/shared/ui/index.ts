1165a9aa12c8348f941c02b5d12a16e89fc83980
nino <dankozero111@gmail.com>
2024-10-13 23:14:26 +0300

    [feat]: add exports

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,5 @@
+export { Color } from './Color/Color';
+export { CustomButton } from './Button/Button';
+export { Text } from './Typography/Typography';
+export { CustomModal } from './Modal/Modal';
+export { CustomInput } from './CustomInput/CustomInmput';
