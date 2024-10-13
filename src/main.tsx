4c1fd78c4cd4080d00e93af3ab0c37aedc602980
nino <dankozero111@gmail.com>
2024-10-13 11:47:30 +0300

    [feat]: just add main app comp

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@shared/styles/index.css';
+
+import { App } from '@app/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
