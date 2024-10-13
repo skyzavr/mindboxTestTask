3c5bf8d7b6f10ed612bb62e76560af3c416b80b7
nino <dankozero111@gmail.com>
2024-10-13 11:27:29 +0300

    [feat]: add feat, when u click on the card it will show the card's data (full title & full description) in midal window

--- src/widgets/TaskCard/ui/Card.tsx
+++ src/widgets/TaskCard/ui/Card.tsx
@@ -1,38 +1,54 @@
+import { useState } from 'react';
 import { Box } from '@mui/material';
 import classNames from 'classnames';
 
+import { ShowCard } from '@widgets/ShowCard';
 import { CardMenu } from '@features/cardMenu';
 import { DoneTask } from '@features/doneTask';
-import { Text } from '@shared/ui';
+import { CustomModal, Text } from '@shared/ui';
 import { CardProps } from '@type/app';
 import { getShortText, setDescription } from '../lib/textHelper';
 
 import { useStyles } from './CardStyles';
 
 export const Card = ({ card }: { card: CardProps }) => {
+  const [showCard, setShowCard] = useState(false);
   const { classes } = useStyles();
   const { title, description, isDone, color, id } = card;
+
+  const titleLen = 30;
+  const descLen = 150;
 
   const wrapperClasses = classNames(
     classes.cardWrapper,
     classes[color],
     isDone ? classes.done : ''
   );
-  const titleNote = getShortText(title, 30);
-  const descNote = getShortText(setDescription(description), 150);
+  const titleNote = getShortText(title, titleLen);
+  const descNote = getShortText(setDescription(description), descLen);
+
+  const onShowCardHandler = () => setShowCard((prev) => !prev);
 
   return (
-    <Box className={wrapperClasses}>
-      <Box className={classes.container}>
-        <Text variant="title">{titleNote}</Text>
-        <Box className={classes.handleWrapper}>
-          <CardMenu id={id} />
+    <>
+      <Box className={wrapperClasses} onClick={onShowCardHandler}>
+        <Box className={classes.container}>
+          <Text variant="title">{titleNote}</Text>
+          <Box className={classes.handleWrapper}>
+            <CardMenu id={id} />
+          </Box>
         </Box>
+        <Box className={`${classes.description}`}>
+          <Text variant={description ? 'desc' : 'placeholder'}>{descNote}</Text>
+        </Box>
+        <DoneTask id={id} init={isDone} />
       </Box>
-      <Box className={`${classes.description}`}>
-        <Text variant={description ? 'desc' : 'placeholder'}>{descNote}</Text>
-      </Box>
-      <DoneTask id={id} init={isDone} />
-    </Box>
+
+      {showCard && (
+        <CustomModal open={showCard} onHandleModal={onShowCardHandler}>
+          <ShowCard {...{ card }} />
+        </CustomModal>
+      )}
+    </>
   );
 };
