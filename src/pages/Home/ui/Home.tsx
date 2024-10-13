5e31f3d1b00dcb36e185e090be55af367e67f53f
nino <dankozero111@gmail.com>
2024-10-13 11:43:37 +0300

    [feat]: add filtratiob by color of cards and type of cards (all/active/done)

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -1,20 +1,28 @@
 import { useSelector } from 'react-redux';
 import { Box } from '@mui/material';
 
-import { RootState } from '@app/store/store';
+import { CardProps } from '@type/app';
 import { Filter } from '@widgets/Filter';
 import { CardsWrapper } from '@widgets/Cards';
 import { Text } from '@shared/ui';
+
 import { getTaskNumberString } from '../lib/textHelper';
+import { getCards, getFilters } from '../lib/selectors';
+import { getListByColor, getListByType } from '../lib/filtration';
 
 import { useStyles } from './HomeStyles';
 
 export const Home = () => {
-  const getData = (state: RootState) => state.cards;
-  const { entities: cards, activeCards } = useSelector(getData);
+  const { entities: cards, activeCards } = useSelector(getCards);
+  const { filterByColor, filterByType } = useSelector(getFilters);
 
   const { classes } = useStyles();
   const activeTaskNumber = getTaskNumberString(activeCards);
+
+  const cardsList = (list: CardProps[]) => {
+    const newList = getListByColor(list, filterByColor);
+    return getListByType(newList, filterByType);
+  };
 
   return (
     <Box className={classes.wrapper}>
@@ -22,7 +30,7 @@ export const Home = () => {
         <Filter />
         <Text variant="plainText">{activeTaskNumber} </Text>
       </Box>
-      <CardsWrapper {...{ cards }} />
+      <CardsWrapper cards={cardsList(cards)} />
     </Box>
   );
 };
