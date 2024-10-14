f26758407bc34a5d760bfe9e6c609af7183693e8
nino <dankozero111@gmail.com>
2024-10-14 18:04:22 +0300

    [build]: add new depend-s and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-
 // https://vitejs.dev/config/
 
+import type { UserConfig as VitestUserConfig } from 'vitest/config';
+declare module 'vite' {
+  export interface UserConfig {
+    test: VitestUserConfig['test'];
+  }
+}
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: 'src/app/testSetup.ts',
+    testMatch: ['src/?(*.)+(test).tsx'],
+    globals: true,
+  },
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, 'src/app'),
